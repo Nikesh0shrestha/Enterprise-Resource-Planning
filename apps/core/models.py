ed0b392b2a9033298082b2b3367145952fcf582a
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-07-29 10:42:42 +0545

    created class Basemodel and company in core models.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Company(BaseModel):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
