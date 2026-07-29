f21f98eeea1e178dce589a416dc06e9d0315bdd8
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-07-29 10:46:11 +0545

    created CompanyAdmin in core admin.py

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Company
 
 # Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "phone",
+        "creaated_at",
+    )
+
+    search_fields = (
+        "name",
+        "email",
+    )
